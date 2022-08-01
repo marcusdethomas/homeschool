f7bfdfa1328fba38530f51a90ce33a9baa71bec5
Marcus Thomas <76545197+marcusdethomas@users.noreply.github.com>
2022-07-31 22:12:58 -0700

    first commit

---
+++ client/src/Views/Main.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Login from '../Components/login';
+import Register from '../Components/register'
+
+const Main = (props) => {
+
+    return (
+        <div>
+            <Login/>
+            <Register/>
+        </div>
+    )
+}
+export default Main;
